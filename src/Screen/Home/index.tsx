ccdc62361ba38f827a51b299eda7eed1ceb71108
Luiz Henrique <7henrique18@gmail.com>
2022-01-12 22:44:55 -0300

    :lipstick: Header

--- src/Screen/Home/index.tsx
+++ src/Screen/Home/index.tsx
@@ -1,9 +1,26 @@
 import * as S from "./styles";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import Logo from "../../assets/logo.svg";
+
+import theme from "../../styles/theme";
 
 export const Home = () => {
   return (
     <S.Container>
-      <S.Title>Home</S.Title>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+
+      <S.Header>
+        <S.HeaderContent>
+          <Logo width={RFValue(118)} height={RFValue(12)} />
+          <S.TotalCars>Total de 12 Carros</S.TotalCars>
+        </S.HeaderContent>
+      </S.Header>
     </S.Container>
   );
 };
--- src/Screen/Home/styles.ts
+++ src/Screen/Home/styles.ts
@@ -1,13 +1,29 @@
 import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { StatusBar } from "react-native";
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background_secondary};
-  align-items: center;
-  justify-content: center;
 `;
 
-export const Title = styled.Text`
-  font-size: 30px;
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+
+  background-color: ${({ theme }) => theme.colors.header};
+  padding: ${`${StatusBar.currentHeight + 32}px`} 24px 32px 24px;
+`;
+
+export const HeaderContent = styled.View`
+  height: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
   font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
 `;
