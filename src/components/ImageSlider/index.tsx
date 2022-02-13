e36025cd4a63b16de4bf01ae0be04f10a8721262
Luiz Henrique <7henrique18@gmail.com>
2022-02-13 15:45:46 -0300

    :lipstick: Image slider

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,28 +1,49 @@
 import * as S from "./styles";
+import { FlatList, ViewToken } from "react-native";
+import { useState, useRef } from "react";
 
 type ImageSliderProps = {
   images: string[];
 };
 
+type ChangeImageProps = {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+};
+
 export const ImageSlider = ({ images }: ImageSliderProps) => {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const indexChanged = useRef((info: ChangeImageProps) => {
+    const index = info.viewableItems[0].index!;
+    setImageIndex(index);
+  });
+
   return (
     <S.Container>
       <S.ImageIndexes>
-        <S.ImageIndex active />
-        <S.ImageIndex />
-        <S.ImageIndex />
-        <S.ImageIndex />
-        <S.ImageIndex />
+        {images.map((_, index) => (
+          <S.ImageIndex active={index === imageIndex} key={String(index)} />
+        ))}
       </S.ImageIndexes>
 
-      <S.CarImageWrapper>
-        <S.CarImage
-          source={{
-            uri: images[0],
-          }}
-          resizeMode="contain"
-        />
-      </S.CarImageWrapper>
+      <FlatList
+        data={images}
+        keyExtractor={(key) => key}
+        renderItem={({ item }) => (
+          <S.CarImageWrapper>
+            <S.CarImage
+              source={{
+                uri: item,
+              }}
+              resizeMode="contain"
+            />
+          </S.CarImageWrapper>
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={indexChanged.current}
+      />
     </S.Container>
   );
 };
