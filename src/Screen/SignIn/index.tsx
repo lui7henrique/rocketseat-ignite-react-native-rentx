ade09869dca7bc81978ec164847e2a5896e638a3
Luiz Henrique <7henrique18@gmail.com>
2022-02-19 17:14:17 -0300

    :wheelchair: Accessibility in Form

--- src/Screen/SignIn/index.tsx
+++ src/Screen/SignIn/index.tsx
@@ -1,4 +1,9 @@
-import { StatusBar, View } from "react-native";
+import {
+  StatusBar,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import { useTheme } from "styled-components";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
@@ -8,36 +13,43 @@ export const SignIn = () => {
   const theme = useTheme();
 
   return (
-    <S.Container>
-      <StatusBar
-        barStyle="dark-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <S.Header>
-        <S.Title>Estamos {"\n"}quase lá</S.Title>
-        <S.Subtitle>Faça seu cadastro{"\n"}de forma rápida e fácil.</S.Subtitle>
-      </S.Header>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <S.Container>
+          <StatusBar
+            barStyle="dark-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          <S.Header>
+            <S.Title>Estamos {"\n"}quase lá</S.Title>
+            <S.Subtitle>
+              Faça seu cadastro{"\n"}de forma rápida e fácil.
+            </S.Subtitle>
+          </S.Header>
 
-      <S.Form>
-        <Input
-          iconName="mail"
-          placeholder="E-mail"
-          keyboardType="email-address"
-          autoCorrect={false}
-        />
-        <Input iconName="lock" placeholder="Senha" isPassword />
-      </S.Form>
-      <S.Footer>
-        <Button title="Login" onPress={() => {}} isLoading={false} />
-        <Button
-          color={theme.colors.background_secondary}
-          textColor={theme.colors.title}
-          title="Criar conta gratuita"
-          onPress={() => {}}
-          isLoading={false}
-        />
-      </S.Footer>
-    </S.Container>
+          <S.Form>
+            <Input
+              iconName="mail"
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCorrect={false}
+              autoCapitalize="none"
+            />
+            <Input iconName="lock" placeholder="Senha" isPassword />
+          </S.Form>
+          <S.Footer>
+            <Button title="Login" onPress={() => {}} isLoading={false} />
+            <Button
+              color={theme.colors.background_secondary}
+              textColor={theme.colors.title}
+              title="Criar conta gratuita"
+              onPress={() => {}}
+              isLoading={false}
+            />
+          </S.Footer>
+        </S.Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
