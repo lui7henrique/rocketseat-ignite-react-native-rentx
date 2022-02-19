457c4410998bb2358940b3b7fcb5133d1766b765
Luiz Henrique <7henrique18@gmail.com>
2022-02-19 20:47:18 -0300

    :bug: Fix StatusBar on Scheduling Screen

--- src/Screen/Scheduling/styles.ts
+++ src/Screen/Scheduling/styles.ts
@@ -15,8 +15,7 @@ export const Header = styled.View`
 
   background-color: ${({ theme }) => theme.colors.header};
 
-  margin-top: ${getStatusBarHeight()}px;
-  padding: 25px;
+  padding: ${getStatusBarHeight() + 24}px 25px;
 `;
 
 export const Title = styled.Text`
